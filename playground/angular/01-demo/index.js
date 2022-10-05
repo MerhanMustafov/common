0c3ba049ce9ca0652d21b73eae9c3b1556c76c7f
unknown <m.o.mustafov@gmail.com>
2022-10-05 23:40:09 +0300

    add

--- playground/angular/01-demo/index.js
+++ playground/angular/01-demo/index.js
@@ -1,42 +1,33 @@
+"use strict";
 // let decimal: number = 5;
 // let message: string = '';
 // let messages: string[] = ['msg 1', 'msg 2'];
-
+exports.__esModule = true;
 // class User {
 //     private name: string
-
 //     constructor(name: string) {
 //         this.name = name;
 //     }
-
 //     sayHello(){
 //         return `${this.name} says hi !`;
 //     }
 // }
-
 // const user = new User('Jhon');
 // console.log(user.sayHello())
-
-class HttpData {
-    
-    public response: undefined;
-    public fulfilled: false;
-
-    constructor(public method: string, public uri: string, public version: string, public message: string ){
+var HttpData = /** @class */ (function () {
+    function HttpData(method, uri, version, message) {
+        this.method = method;
+        this.uri = uri;
+        this.version = version;
+        this.message = message;
         this.method = method;
         this.uri = uri;
         this.version = version;
         this.message = message;
         this.response;
         this.fulfilled;
-
     }
-
-
-}
-
-let myData = new HttpData('GET', 'http://google.com', 'HTTP/1.1', '')
-console.log(myData)
-export {};
-
-export {};
+    return HttpData;
+}());
+var myData = new HttpData('GET', 'http://google.com', 'HTTP/1.1', '');
+console.log(myData);
